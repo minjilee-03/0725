3a936235a3cfb157e3f6e177720b1179227af9d2
minjilee-03 <56942604+minjilee-03@users.noreply.github.com>
2021-07-25 22:13:07 +0900

    main-visual 슬라이드 코드 추가

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
+$(document).ready(function () {
+    $(".main-visual-slick-slides").slick({
+      autoplay: false,
+      cssEase: 'ease-in-out',
+      arrows: false,
+      dots: true
+    });
+  });
 $.noConflict();
